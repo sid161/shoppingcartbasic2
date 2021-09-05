95cfa12ec82636bedc47296858aabd2ce338bf68
siddharth <sharma.siddharth1610@gmail.com>
2021-09-05 14:40:14 +0530

    block3

---
+++ my-app/src/data.js
@@ -0,0 +1,59 @@
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'MacBook',
+        price: 1400,
+        image: 'https://picsum.photos/id/180/2400/1600',
+      },
+      {
+        id: '2',
+        name: 'Old Car',
+        price: 2400,
+        image: 'https://picsum.photos/id/111/4400/2656',
+      },
+      {
+        id: '3',
+        name: 'W Shoes',
+        price: 1000,
+        image: 'https://picsum.photos/id/21/3008/2008',
+      },
+      {
+        id: '4',
+        name: ' Furniture',
+        price: 2000,
+        image: 'https://picsum.photos/id/111/4400/2656',
+      },
+      {
+        id: '5',
+        name: ' Fan',
+        price: 800,
+        image: 'https://picsum.photos/id/180/2400/1600',
+      },
+      {
+        id: '6',
+        name: ' Cycle',
+        price: 1800,
+        image: 'https://picsum.photos/id/21/3008/2008',
+      },
+      {
+        id: '7',
+        name: 'MacBook',
+        price: 1400,
+        image: 'https://picsum.photos/id/180/2400/1600',
+      },
+      {
+        id: '8',
+        name: 'Old Car',
+        price: 2400,
+        image: 'https://picsum.photos/id/111/4400/2656',
+      },
+      {
+        id: '9',
+        name: 'W Shoes',
+        price: 1000,
+        image: 'https://picsum.photos/id/21/3008/2008',
+      },
+    ],
+  };
+  export default data;
